90c71ba9ae885d890016f4da30996ab923b8f985
JinDaifer <thedaifer1@gmail.com>
2024-11-12 01:06:37 +0100

    Updates

---
+++ precache-manifest.d372c41e548c1622b53e587eb5747af6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c702cf08664f07d2fde893ff1bf9b3",
+    "url": "/reactjs.git/index.html"
+  },
+  {
+    "revision": "155ba30f88455d1f8314",
+    "url": "/reactjs.git/static/css/main.ca0647a1.chunk.css"
+  },
+  {
+    "revision": "3a029e42e2c6ec5f2770",
+    "url": "/reactjs.git/static/js/2.c8b25181.chunk.js"
+  },
+  {
+    "revision": "67ae3e0212c3a537d86a1f099983ebeb",
+    "url": "/reactjs.git/static/js/2.c8b25181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5862fbf212b6574eb65",
+    "url": "/reactjs.git/static/js/3.bc5711c0.chunk.js"
+  },
+  {
+    "revision": "155ba30f88455d1f8314",
+    "url": "/reactjs.git/static/js/main.da4c413f.chunk.js"
+  },
+  {
+    "revision": "61f5b41173dc5dfa76cf",
+    "url": "/reactjs.git/static/js/runtime-main.0de08787.js"
+  }
+]);
