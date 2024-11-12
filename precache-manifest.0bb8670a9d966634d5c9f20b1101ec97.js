a9a70a0f2b0ddb94f3f19b737ac1a6a3fcd22ce9
JinDaifer <thedaifer1@gmail.com>
2024-11-12 01:47:43 +0100

    Updates

--- precache-manifest.0bb8670a9d966634d5c9f20b1101ec97.js
+++ precache-manifest.0bb8670a9d966634d5c9f20b1101ec97.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cd95ba6f04c4f513d2cf06973c094b18",
+    "revision": "308a234526ba70f0fdcc8e0d6c782494",
     "url": "/reactjs.git/index.html"
   },
   {
-    "revision": "89b9702095239871e8c1",
+    "revision": "a7095de013fd2a9f9d39",
     "url": "/reactjs.git/static/css/main.ca0647a1.chunk.css"
   },
   {
-    "revision": "32735bfd04b73f03cd2a",
-    "url": "/reactjs.git/static/js/2.b1bf9d46.chunk.js"
+    "revision": "3a029e42e2c6ec5f2770",
+    "url": "/reactjs.git/static/js/2.c8b25181.chunk.js"
   },
   {
     "revision": "67ae3e0212c3a537d86a1f099983ebeb",
-    "url": "/reactjs.git/static/js/2.b1bf9d46.chunk.js.LICENSE.txt"
+    "url": "/reactjs.git/static/js/2.c8b25181.chunk.js.LICENSE.txt"
   },
   {
     "revision": "e5862fbf212b6574eb65",
     "url": "/reactjs.git/static/js/3.bc5711c0.chunk.js"
   },
   {
-    "revision": "89b9702095239871e8c1",
-    "url": "/reactjs.git/static/js/main.88600f67.chunk.js"
+    "revision": "a7095de013fd2a9f9d39",
+    "url": "/reactjs.git/static/js/main.54aecfa9.chunk.js"
   },
   {
     "revision": "61f5b41173dc5dfa76cf",
