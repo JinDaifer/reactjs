12e860c418a2b33db36dc467159aa8d5dc989533
JinDaifer <thedaifer1@gmail.com>
2024-11-12 02:05:35 +0100

    pa

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, useLocation , Route, Routes} from 'react-router-dom';
 import './App';
 import Home from './Home';
 import Chess from './Chess';
 
+function RouteWithLocation() {
+  const location = useLocation();
+  console.log("Ruta actual:", location.pathname);  // Muestra la ruta actual en la consola
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
+      <RouteWithLocation />
       <Routes>
-        <Route path= {process.env.PUBLIC_URL +'/reactjs'} element={<Home/>}>
-        </Route>
-        <Route path={process.env.PUBLIC_URL +'/'} element={<Home/>}>
-        </Route>
-        <Route path={process.env.PUBLIC_URL +'/Chess'} element={<Chess/>}>
-        </Route>
+        <Route path='/reactjs' element={<Home />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/Chess' element={<Chess />} />
       </Routes>
     </BrowserRouter>
   );
